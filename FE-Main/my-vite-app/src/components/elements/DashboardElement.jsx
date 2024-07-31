04f3ce2fc2fd16dab63f7aaf909f9f7b3a8ff457
KelvinTjoe <115490689+KelvinTjoe@users.noreply.github.com>
2024-07-31 11:42:28 +0700

    tambahin edit employee di dashboard

--- FE-Main/my-vite-app/src/components/elements/DashboardElement.jsx
+++ FE-Main/my-vite-app/src/components/elements/DashboardElement.jsx
@@ -30,7 +30,7 @@ export default function DashboardElement() {
           onMouseLeave={() => handleHover(null)}
           className={`my-info ${
             hoveredButton === "my-info" ? "my-info-hover" : ""
-          } w-[280px] rounded-r-lg flex items-center pl-8 py-4 cursor-pointer hover:bg-[#191a39]`}
+          } w-[280px] rounded-r-lg flex items-center pl-8 pb-4 cursor-pointer hover:bg-[#191a39]`}
           onClick={() => navigate("/my-info")}
         >
           
@@ -40,6 +40,8 @@ export default function DashboardElement() {
 
           <p className="my-auto text-white ml-5 text-[20px]">My Info</p>
         </div>
+
+        <hr class="h-1 my-2 bg-white dark:bg-gray-700"></hr>
 
         <div
           onMouseOver={() => handleHover("home")}
@@ -69,6 +71,23 @@ export default function DashboardElement() {
           </svg>
 
           <p className="my-auto text-white ml-5 text-[20px]">Add Employee</p>
+        </div>
+
+        <div
+          onMouseOver={() => handleHover("edit-employee")}
+          onMouseLeave={() => handleHover(null)}
+          className={`edit-employee ${
+            hoveredButton === "edit-employee" ? "edit-employee-hover" : ""
+          } w-[280px] rounded-r-lg flex items-center pl-8 py-4 cursor-pointer hover:bg-[#191a39]`}
+          onClick={() => navigate("/edit-employee")}
+        >
+          
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-10 stroke-white">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+          </svg>
+
+
+          <p className="my-auto text-white ml-5 text-[20px]">Edit Employee List</p>
         </div>
 
         <div
