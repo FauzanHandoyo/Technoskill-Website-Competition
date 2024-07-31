23f42cbedfe8f2c9ace8ad3191559eead7b5228f
KelvinTjoe <115490689+KelvinTjoe@users.noreply.github.com>
2024-07-31 09:57:27 +0700

    mengganti image yang dipakai di dashboard

--- FE-Main/my-vite-app/src/components/elements/DashboardElement.jsx
+++ FE-Main/my-vite-app/src/components/elements/DashboardElement.jsx
@@ -30,10 +30,14 @@ export default function DashboardElement() {
           onMouseLeave={() => handleHover(null)}
           className={`my-info ${
             hoveredButton === "my-info" ? "my-info-hover" : ""
-          }  w-[280px] rounded-r-lg flex items-center pl-4 py-4 cursor-pointer hover:bg-[#191a39]`}
+          } w-[280px] rounded-r-lg flex items-center pl-8 py-4 cursor-pointer hover:bg-[#191a39]`}
           onClick={() => navigate("/my-info")}
         >
-          <img src={myInfoIcon} alt="My Info" />
+          
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-10 stroke-white " >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+          </svg>
+
           <p className="my-auto text-white ml-5 text-[20px]">My Info</p>
         </div>
 
@@ -42,10 +46,13 @@ export default function DashboardElement() {
           onMouseLeave={() => handleHover(null)}
           className={`home ${
             hoveredButton === "home" ? "home-hover" : ""
-          } w-[280px] rounded-r-lg flex items-center pl-4 py-4 cursor-pointer hover:bg-[#191a39]`}
+          } w-[280px] rounded-r-lg flex items-center pl-8 py-4 cursor-pointer hover:bg-[#191a39]`}
           onClick={() => navigate("/home")}
         >
-          <img src={homeIcon} alt="Home" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"  class="size-10 stroke-white">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>
+
           <p className="my-auto text-white ml-5 text-[20px]">Home</p>
         </div>
 
@@ -54,10 +61,13 @@ export default function DashboardElement() {
           onMouseLeave={() => handleHover(null)}
           className={`add-employee ${
             hoveredButton === "add-employee" ? "add-employee-hover" : ""
-          } w-[280px] rounded-r-lg flex items-center pl-4 py-4 cursor-pointer hover:bg-[#191a39]`}
+          } w-[280px] rounded-r-lg flex items-center pl-8 py-4 cursor-pointer hover:bg-[#191a39]`}
           onClick={() => navigate("/add-employee")}
         >
-          <img src={addEmployeeIcon} alt="Add Employee" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-10 stroke-white">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+          </svg>
+
           <p className="my-auto text-white ml-5 text-[20px]">Add Employee</p>
         </div>
 
@@ -66,10 +76,13 @@ export default function DashboardElement() {
           onMouseLeave={() => handleHover(null)}
           className={`login ${
             hoveredButton === "login" ? "login-hover" : ""
-          } w-[280px] rounded-r-lg flex items-center pl-4 py-4 cursor-pointer hover:bg-[#191a39]`}
+          } w-[280px] rounded-r-lg flex items-center pl-8 py-4 cursor-pointer hover:bg-[#191a39]`}
           onClick={() => navigate("/login")}
         >
-          <img src={loginIcon} alt="Login" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"  class="size-10 stroke-white">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+          </svg>
+
           <p className="my-auto text-white ml-5 text-[20px]">Login</p>
         </div>
       </div>
